349764c52faf16fc4062c80d6194c67425b131a2
Ryan Howard <ryanghoward@protonmail.com>
2022-06-03 12:57:39 -0700

    Still broken af. Tryna fix. Plz halp

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,25 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/BlogCard.module.css";
 
-export default function BlogPosts({
-  title,
-  author,
-  coverPhoto,
-  datePublished,
-  slug,
-}) {
+function BlogCard({ title, author, coverPhoto, datePublished, slug }) {
   return (
     <div className={styles.card}>
-      <Link href={"/posts/" + slug}>
+      <Link href={`/posts/${slug}`}>
         <div className={styles.imgContainer}>
-          <img src={coverPhoto.url} alt='idk' />
+          <Image layout='fill' src={coverPhoto.url} alt='idk' />
         </div>
       </Link>
       <div className={styles.text}>
         <h2>{title}</h2>
         <div className={styles.details}>
           <div className={styles.author}>
-            <img src={author.avatar.url} alt='who cares' />
+            <Image src={author.avatar.url} alt={author.name} />
             <h3>{author.name}</h3>
           </div>
           <div className={styles.date}>
@@ -30,3 +25,5 @@ export default function BlogPosts({
     </div>
   );
 }
+
+export default BlogCard;
